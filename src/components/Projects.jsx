519df040286b6e25b0a6a579d005fa1112086eac
Sarah Ubiebi <ubiebisarah6@gmail.com>
2025-06-13 15:31:13 +0100

    Fixed an issue

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,15 @@
 import img3 from "../assets/image3.JPG";
 import img4 from "../assets/image4.JPG";
 import { IoIosArrowForward } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const Projects = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/projects");
-  };
+  // const navigate = useNavigate();
+  // const handleClick = () => {
+  //   navigate("/projects");
+  // };
   return (
     <section
       id="project"
@@ -51,16 +52,18 @@ const Projects = () => {
         </p>
       </div>
       {/* <button className=""> */}
-
-      <p
+    <Link to={"/projects"} onClick={() => window.scroll(0, 0)}>
+    <p
         className="flex bg-white gap-2 border px-2 py-1 rounded-xl mt-4 w-[150px] text-[#0F2917] cursor-pointer"
-        onClick={handleClick}
+        
       >
         View Projects
         <span className="text-xl mt-0.5">
           <IoIosArrowForward />
         </span>
       </p>
+    </Link>
+      
     </section>
   );
 };
